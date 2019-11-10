cb1ab0731ad5bd4671d5d55bd7fd4c204330fef1
0x587 <3225678419@qq.com>
2019-11-10 08:43:30 +0800

    班级历史成绩

--- AnalysisData/Class.py
+++ AnalysisData/Class.py
@@ -1,6 +1,7 @@
-from pyecharts.charts import Funnel
+from pyecharts.charts import Funnel, Timeline, Bar
 from pyecharts import options as opts
 from app.models import *
+from pyecharts.globals import ThemeType
 
 
 def class_grade_distributed(class_index: int) -> Funnel:
@@ -9,7 +10,7 @@ def class_grade_distributed(class_index: int) -> Funnel:
     for student in students:
         data[student.analysis[0].get_level()] += 1
     chart = (
-        Funnel(init_opts=opts.InitOpts(width='100%', height='400px'))
+        Funnel(init_opts=opts.InitOpts(width='100%', height='400px',theme=ThemeType.VINTAGE))
             .add('level', [list(z) for z in zip(data.keys(), data.values())],
                  sort_="none", gap=5, label_opts=opts.LabelOpts())
     )
@@ -21,3 +22,25 @@ def class_type(class_index: int) -> str:
         return 'Experiment class'
     else:
         return 'Ordinary class'
+
+
+def class_history_grade(class_index: int) -> Timeline:
+    tl = Timeline(init_opts=opts.InitOpts(theme=ThemeType.VINTAGE))
+    for test in Test.query.all():
+        cag: ClassAverageGrade = ClassAverageGrade.query.filter(
+            ClassAverageGrade.test_time == test.test_time,
+            ClassAverageGrade.class_index == class_index,
+        ).first()
+        grades = [cag.chinese, cag.match, cag.english,
+                  cag.physics, cag.chemistry, cag.biology, ]
+        rankings = [cag.chinese_ranking, cag.match_ranking, cag.english_ranking,
+                    cag.physics_ranking, cag.chemistry_ranking, cag.biology_ranking, ]
+        bar = (
+            Bar(init_opts=opts.InitOpts(theme=ThemeType.VINTAGE))
+                .add_xaxis(['语文', '数学', '英语', '物理', '化学', '生物'])
+                .add_yaxis('Grade', grades)
+                .add_yaxis('Ranking', rankings)
+                .set_global_opts(title_opts=opts.TitleOpts(test.test_name))
+        )
+        tl.add(bar, test.test_time)
+    return tl
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from app import app
 from flask import render_template, request
 from class_info import *
-from AnalysisData.Class import class_grade_distributed, class_type
-from temp import personal_history_grade
+from AnalysisData.Class import class_grade_distributed, class_type, class_history_grade
+from AnalysisData.Student import personal_history_grade
 
 
 @app.route('/')
@@ -24,6 +24,7 @@ def class_info(class_index):
         student_infos.append({'index': s[0], 'name': s[1], 'details': details,
                               'full_detail': None})
     return render_template('class_info.html',
+                           history_chart=class_history_grade(class_index).render_embed(),
                            class_infos=class_infos,
                            distributed=class_grade_distributed(class_index).render_embed(),
                            students=student_infos, )

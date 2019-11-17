85755b3df2743e81f366b1d72c17cf187e3960c6
0x587 <3225678419@qq.com>
2019-11-17 17:54:12 +0800

    personal_history_grade方法

--- AnalysisData/Student.py
+++ AnalysisData/Student.py
@@ -1,4 +1,9 @@
+from pyecharts.charts import Line, Bar
+from pyecharts import options as opts
 from app.models import *
+from pyecharts.globals import ThemeType
+
+
 def student_type(class_index: int) -> str:
     """
     返回学生类型。
@@ -47,4 +52,40 @@ def set_level():
         db.session.commit()
 
 
-set_level()
+def personal_history_grade(student: Student) -> Line:
+    """
+    生成该考生历次成绩折线图。
+    :param student:Student
+    :return: line:Line
+    """
+    s = student
+    key = [t.test_name for t in Test.query.all()]
+    values = [g.total_ranking for g in s.grades]
+    current_data: list = values.copy()
+    current_data[-1] = None
+    current_line = (Line(init_opts=opts.InitOpts(theme=ThemeType.WONDERLAND))
+                    .add_xaxis(key)
+                    .add_yaxis('A', current_data, is_smooth=True, is_connect_nones=True,
+                               # label_opts=opts.LabelOpts(is_show=False),
+                               markpoint_opts=opts.MarkPointOpts(
+                                   data=[opts.MarkPointItem(symbol=r'image:///static/top.png',
+                                                            symbol_size=60, type_='max'),
+                                         opts.MarkPointItem(symbol=r'image:///static/low.png',
+                                                            symbol_size=40, type_='min')]
+                               )))
+    future_data: list = [None for _ in range(len(values))]
+    future_data[-2:] = values[-2:]
+    future_line = (Line()
+                   .add_xaxis(key)
+                   .add_yaxis('A', future_data, is_smooth=True, is_connect_nones=True,
+                              linestyle_opts=opts.LineStyleOpts(type_="dashed"),
+                              markpoint_opts=opts.MarkPointOpts(
+                                  data=[opts.MarkPointItem(
+                                      symbol='circle',
+                                      name="下次预测", coord=[key[-1], values[-1]], value=values[-1])]
+                              ),
+                              ))
+    current_line.set_global_opts(title_opts=opts.TitleOpts(title='历次成绩',
+                                                           subtitle='预测结果使用{}次方程拟合'.
+                                                           format('二')))
+    return current_line.overlap(future_line)

305f0e57ca440a07a9ee982a0b9b2fe2494634ed
0x587 <3225678419@qq.com>
2019-11-20 19:21:07 +0800

    该方法已移至LoadData.AnalysisStudent

--- AnalysisData/Student.py
+++ AnalysisData/Student.py
@@ -31,28 +31,6 @@ def student_subject(class_index: int) -> str:
         return '理科'
 
 
-def set_level():
-    """
-    计算所有学生的平均排名。
-    :return: None
-    """
-    grades = Test.query.order_by(Test.test_time.desc()).first().grades
-    liberal_arts_count = len([g for g in grades if g.subject == '文科'])
-    science_count = len([g for g in grades if g.subject == '理科'])
-    for student in Student.query.all():
-        ranking_count = 0
-        for grade in student.grades:
-            if grade.subject == '文科':
-                ranking_count += grade.total_ranking / liberal_arts_count
-            else:
-                ranking_count += grade.total_ranking / science_count
-        ranking_avg = ranking_count / len(student.grades)
-        new = AnalysisStudent(ranking_avg)
-        new.student = student
-        db.session.add(new)
-        db.session.commit()
-
-
 def personal_history_grade(student: Student) -> Line:
     """
     生成该考生历次成绩折线图。

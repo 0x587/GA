37a1dd51df01c39f49fb0cd8c15f54786a111297
0x587 <3225678419@qq.com>
2019-11-24 16:28:08 +0800

    文档change

--- GradeRanking/__init__.py
+++ GradeRanking/__init__.py
@@ -29,7 +29,6 @@ def grade2ranking(test: Test, grade: int, subject: str = 'total', subject_type: 
 
 def grade2ranking_for_class(grade_id: int, subject: str = 'total') -> int:
     """
-    查找本次考试此分数对应(或最相近)的排名。
     查找本次考试此分数对应(或最相近)的班级排名。
     :param grade_id: Grade对象id
     :param subject:查找的科目

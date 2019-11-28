f3a19552882ce268bb042b93292ffe434515469b
0x587 <3225678419@qq.com>
2019-11-28 19:04:40 +0800

    test_grade_distributed_chart

--- AnalysisData/Test.py
+++ AnalysisData/Test.py
@@ -17,3 +17,33 @@ def test_grade_distributed(test_time: int) -> list:
     for i in range(15):
         result[14 - i]['key'] = '{}分—{}分段'.format(i * 10, i * 10 + 10)
     return result
+
+
+def test_grade_distributed_chart(test_time: int, subject: str = 'total') -> Bar:
+    if subject != 'total':
+        data = test_grade_distributed(test_time)
+    else:
+        totals = [_[0] for _ in db.session.query(StudentGrade.total).filter_by(
+            test_time=test_time, subject='理科').all()]
+        data = [{} for _ in range(50)]
+        for i in range(50):
+            data[49 - i]['key'] = '{}分-{}分段'.format(i * 15, i * 15 + 15)
+            data[49 - i]['total'] = len([_ for _ in totals if i * 15 < _ < i * 15 + 15])
+    bar = (
+        Bar(init_opts=opts.InitOpts(theme=ThemeType.VINTAGE))
+            .add_yaxis('人数', [_[subject] for _ in data])
+            .add_xaxis([d['key'] for d in data])
+            .set_global_opts(
+            xaxis_opts=opts.AxisOpts(
+                axislabel_opts=opts.LabelOpts(
+                    rotate=-15
+                )
+            ),
+            datazoom_opts=[opts.DataZoomOpts(type_='slider',
+                                             range_start=5, range_end=55),
+                           opts.DataZoomOpts(type_="inside")],
+            legend_opts=opts.LegendOpts(is_show=False)
+        )
+    )
+    return bar
+

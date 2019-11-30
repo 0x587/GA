064a3e450641892f0325c644ac3813002d777fb1
0x587 <3225678419@qq.com>
2019-11-30 20:59:47 +0800

    no message

--- app/chart_routers.py
+++ app/chart_routers.py
@@ -1,8 +1,9 @@
 from app import app
 from AnalysisData.Class import class_history_grade, class_grade_distributed
 from AnalysisData.Student.charts import student_grade_radar, student_grade_compared
-from AnalysisData.Test import test_grade_distributed_chart
+from AnalysisData.Test import test_grade_distributed_chart, test_avg_grade_compare
 from app.models import *
+import json
 
 
 @app.route('/charts/history_grade_chart/<int:class_index>')
@@ -32,3 +33,12 @@ def ranking_radar_chart(grade_id: int):
 @app.route('/charts/test_grade_distributed_chart/<int:test_time>/<string:subject>')
 def grade_distributed_chart(test_time: int, subject: str):
     return test_grade_distributed_chart(test_time, subject).dump_options_with_quotes()
+
+
+@app.route('/charts/test_grade_avg_compare_chart/<int:test_time>')
+def grade_avg_compare_chart(test_time: int):
+    data = test_avg_grade_compare(test_time)
+    result = {}
+    for key, value in data.items():
+        result[key] = json.loads(value.dump_options())
+    return json.dumps(result)

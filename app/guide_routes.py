d5967ba7f37fb1e42a1082290eadfab6b00ad689
0x587 <3225678419@qq.com>
2019-12-07 22:53:30 +0800

    学生搜索框

--- app/guide_routes.py
+++ app/guide_routes.py
@@ -1,8 +1,14 @@
 from app import app
-from flask import render_template, send_file
+from flask import render_template, request
 from app.api_routers import *
 
 
 @app.route('/choose_Identity')
 def choose_identity():
     return render_template('choose_identity.html')
+
+
+@app.route('/choose_Identity/<string:user_identity>')
+def identity(user_identity: str):
+    if user_identity == 'student':
+        return render_template('guide/student.html')

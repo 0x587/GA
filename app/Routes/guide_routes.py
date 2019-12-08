ec858de8c3db37087d871038e71eff6d5a19e507
0x587 <3225678419@qq.com>
2019-12-08 18:29:51 +0800

    teacher

--- app/Routes/guide_routes.py
+++ app/Routes/guide_routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, abort
 from app.Routes.api_routers import *
 
 
@@ -11,3 +11,7 @@ def choose_identity():
 def identity(user_identity: str):
     if user_identity == 'student':
         return render_template('guide/student_select.html')
+    elif user_identity == 'teacher':
+        return render_template('guide/teacher_select.html')
+    else:
+        abort(404)

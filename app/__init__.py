1970508ae6b9715568eee561f679a077a8c0fd92
0x587 <3225678419@qq.com>
2019-12-08 20:42:10 +0800

    no message

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from my_config import Config
 import os
 
@@ -12,8 +13,10 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+login = LoginManager(app)
 if not app.debug:
     if not os.path.exists('logs'):
         os.mkdir('logs')
 
 from app import models, Routes
+import User.UserModel
